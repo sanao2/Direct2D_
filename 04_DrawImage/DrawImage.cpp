3fbef257272d29a1f2b5892d3ade36d1cab48745
Sanao2 <SanaoAiza1102@gmail.com>
2025-06-02 17:08:47 +0900

    #분석

--- 04_DrawImage/DrawImage.cpp
+++ 04_DrawImage/DrawImage.cpp
@@ -25,8 +25,10 @@ ComPtr<ID2D1Bitmap1> g_d2dBitmapTarget;
 
 // For ImageDraw
 ComPtr<IWICImagingFactory> g_wicImagingFactory;
-ComPtr<ID2D1Bitmap1> g_d2dBitmapFromFile;
+//PEG나 PNG, BMP, GIF 등 적절한 디코더(IWICBitmapDecoder)를 내부적으로 선택하고
+//해당 파일을 해석(디코딩)하여 메모리상의 비트맵 프레임을 얻을 수 있게 함. 
 
+ComPtr<ID2D1Bitmap1> g_d2dBitmapFromFile;
 
 UINT g_width = 1024;
 UINT g_height = 768;
@@ -38,9 +40,9 @@ void Uninitialize();
 // WIC를 통해 PNG 등을 로드하여 ID2D1Bitmap1**으로 반환
 HRESULT CreateBitmapFromFile(const wchar_t* path, ID2D1Bitmap1** outBitmap)
 {
-	ComPtr<IWICBitmapDecoder>     decoder;
-	ComPtr<IWICBitmapFrameDecode> frame;
-	ComPtr<IWICFormatConverter>   converter;
+	ComPtr<IWICBitmapDecoder>     decoder;    // 이미지 포멧 디코더. 스트림 데이터를 디코딩하여 프레임 단위의 비트맵 객체로 변환. 즉, 픽셀 정보를 추출.  
+	ComPtr<IWICBitmapFrameDecode> frame;	  // 이미지를 그릴 프레임 
+	ComPtr<IWICFormatConverter>   converter;  // WIC(Windows Imaging Component)에서 읽어들인 비트맵 소스를 다른 픽셀 포맷으로 변환
 
 	// ① 디코더 생성
 	HRESULT hr = g_wicImagingFactory->CreateDecoderFromFilename(
@@ -57,18 +59,19 @@ HRESULT CreateBitmapFromFile(const wchar_t* path, ID2D1Bitmap1** outBitmap)
 
 	// ④ GUID_WICPixelFormat32bppPBGRA로 변환
 	hr = converter->Initialize(
-		frame.Get(),
-		GUID_WICPixelFormat32bppPBGRA,
-		WICBitmapDitherTypeNone,
-		nullptr,
-		0.0f,
-		WICBitmapPaletteTypeCustom
+		frame.Get(),					// 어떤 비트맵(프레임)을 변환할 것인가?”를 지정하는 역할
+		GUID_WICPixelFormat32bppPBGRA,  // 어떤 픽셀 포맷으로 변환할 것인지”를 지정하는 GUID
+		WICBitmapDitherTypeNone,        // 픽셀 간 경계를 부드럽게 표현 여부 결정 // 블렌딩을 말하는 것. 
+		nullptr,						// 어떤 색상 팔레트 기반 포맷을 사용할 것인지 지정. nullptr이면 팔레트 없음 
+		0.0f,							// 알파 임계값
+		WICBitmapPaletteTypeCustom	    // 원본 이미지에서 팔레트 기반 변환 시, 어떤 팔레트 전략을 쓸 것인지”를 지정 // 고정 팔레트와 동적 팔레트 전략이 존재 
 	);
+	// HRESULT를 반환합니다. 성공하면 S_OK 또는 S_FALSE, 실패하면 적절한 에러 코드(E_FAIL, E_INVALIDARG 등)
 	if (FAILED(hr)) return hr;
 
 	// ⑤ Direct2D 비트맵 속성 (premultiplied alpha, B8G8R8A8_UNORM)
 	D2D1_BITMAP_PROPERTIES1 bmpProps = D2D1::BitmapProperties1(
-		D2D1_BITMAP_OPTIONS_NONE,
+		D2D1_BITMAP_OPTIONS_NONE,// 비트맵 옵션. GDI 와 함께 쓸 때에는 D2D1_BITMAP_OPTIONS_GDI_COMPATIBLE 사용 
 		D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED)
 	);
 
